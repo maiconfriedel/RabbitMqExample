2d5cf0ffc0a8c4b7d0c8fd36d3d230e15d2912f7
Maicon Friedel <maicon.friedel@grupokyly.com.br>
2024-01-19 15:55:56 -0300

    Add project files.

---
+++ RabbitMQ.Consumer/Consumer.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost", UserName = "admin", Password = "admin" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var queueName = "logs";
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received: {message}");
+};
+
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.ReadLine();
---
+++ RabbitMq/Producer.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost", UserName = "admin", Password = "admin" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+int count = 0;
+
+while (true)
+{
+    var message = $"info: Hello World! {DateTime.Now}";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "logs",
+                         routingKey: count == 5 ? "error" : string.Empty,
+                         basicProperties: null,
+                         body: body);
+
+    Console.WriteLine($" [x] Sent {message}");
+    
+    if (count == 5) count = 0;
+    count++;
+
+    await Task.Delay(TimeSpan.FromSeconds(2));
+}
